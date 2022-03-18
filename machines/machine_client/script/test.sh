fc84e3c4256330d48b166f6808675af93a4822fc
IlyaG01 <golovakov.ilya@gmail.com>
2022-03-18 22:04:26 +0300

    Upgrade my work

--- machines/machine_client/script/test.sh
+++ machines/machine_client/script/test.sh
@@ -1,3 +1,3 @@
 curl -X GET 192.168.29.10:5000/
-curl -X POST 192.168.29.10:5000/
+curl -X PUT -d 'data=linux_is_the_best' http://192.168.29.10:5000/data
 curl -X POST -d 'username=Ilya' http://192.168.29.10:5000/name
